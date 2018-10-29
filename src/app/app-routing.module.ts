7b784dd610f662af34ca9ea631516960412e23ff
Jessica Brandt <jessenbrandt@gmail.com>
2018-10-29 16:26:02 -0400

    inital nav bar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ContactComponent } from "./contact/contact.component";
+import { AuthComponent } from "./auth/auth.component";
+import { AboutComponent } from "./about/about.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "", redirectTo:"/home", pathMatch: "full"},
+  {path: "home", component: HomeComponent},
+  {path: "contact", component: ContactComponent},
+  {path: "auth", component: AuthComponent},
+  {path: "about", component: AboutComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { HomeComponent } from './home/home.component';
+import { AuthComponent } from './auth/auth.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+// import {MatMenuModule} from '@angular/material/menu';
+
+// import {MatButtonModule, MatCheckboxModule, MatMenu} from '@angular/material';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    HomeComponent,
+    AuthComponent,
+    AboutComponent,
+    ContactComponent,
+    // MatMenuModule
   ],
   imports: [
     BrowserModule,
