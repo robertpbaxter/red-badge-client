64e67a623dccee1aaafd6773ad0b3e9a17cdfb7e
Robert Baxter <roer.baxter@gmail.com>
2018-10-30 11:14:45 -0400

    pre-pull test

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from "@angular/core";
+import { User } from "../user";
+import { AuthService } from "src/app/auth.service";
+
+@Component({
+  selector: "app-login",
+  templateUrl: "./login.component.html",
+  styleUrls: ["./login.component.css"]
+})
+export class LoginComponent implements OnInit {
+  constructor(private authService: AuthService) {}
+
+  login(email: string, password: string): void {
+    this.authService
+      .login({ email, password } as User)
+      .subscribe(credentials => {
+        console.log(credentials);
+        // localStorage.setItem("token", credentials.sessionToken);
+        // localStorage.setItem("role", credentials.permission);
+      });
+  }
+
+  ngOnInit() {}
+}
