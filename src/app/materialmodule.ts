9dfb8489d2edede5cff9565828cefb56174f5bb3
Jessica Brandt <jessenbrandt@gmail.com>
2018-10-30 11:07:08 -0400

    basic nav bar with material

---
+++ src/app/materialmodule.ts
@@ -0,0 +1,10 @@
+import {MatButtonModule, MatCheckboxModule, MatIconModule, MatToolbarModule} from '@angular/material';
+import { NgModule } from '@angular/core';
+
+@NgModule({
+    imports: [MatButtonModule, MatCheckboxModule, MatIconModule, MatToolbarModule],
+
+    exports: [MatButtonModule, MatCheckboxModule, MatIconModule, MatToolbarModule],
+})
+
+export class MaterialModule { }
