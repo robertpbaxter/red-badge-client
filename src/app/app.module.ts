7242b550059040278a60326fceb21e3c93b014a1
[IngeborgSlegers] <[ingeborgfreda@gmail.com]>
2018-10-31 10:20:05 -0400

    initial signup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -32,7 +32,7 @@ import { HttpClientModule } from "@angular/common/http";
     MaterialModule,
     BrowserAnimationsModule,
     HttpClientModule,
-    FormsModule
+    FormsModule,
     // MatMenuModule
   ],
   providers: [],
