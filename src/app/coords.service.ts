c42edf3d1a97faf209279da5cf93edb89dae4ece
Robert Baxter <roer.baxter@gmail.com>
2018-11-01 14:37:36 -0400

    completed coords service

---
+++ src/app/coords.service.ts
@@ -0,0 +1,66 @@
+import { Injectable } from "@angular/core";
+import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { Observable, of } from "rxjs";
+import { catchError } from "rxjs/operators";
+import { Coords } from "./coords";
+
+@Injectable({
+  providedIn: "root"
+})
+export class CoordsService {
+  token: string = localStorage.getItem("token");
+
+  constructor(private http: HttpClient) {}
+
+  httpOptions = {
+    headers: new HttpHeaders({
+      "Content-Type": "application/json",
+      Authorization: this.token
+    })
+  };
+
+  private coordsUrl = "https://team-buh-gok-server.herokuapp.com/api/coords";
+
+  private handleError<T>(operation = "operation", result?: T) {
+    return (error: any): Observable<T> => {
+      console.error(error);
+      return of(result as T);
+    };
+  }
+
+  newCoords(coords: Coords): Observable<any> {
+    return this.http
+      .post<Coords>(this.coordsUrl, coords, this.httpOptions)
+      .pipe(catchError(this.handleError<Coords>("newCoords")));
+  }
+
+  getAllCoords(): Observable<Coords[]> {
+    return this.http
+      .get<Coords[]>(this.coordsUrl, this.httpOptions)
+      .pipe(catchError(this.handleError("fetchAllCoords", [])));
+  }
+
+  getUserCoords(): Observable<Coords[]> {
+    return this.http
+      .get<Coords[]>(`${this.coordsUrl}/owner`, this.httpOptions)
+      .pipe(catchError(this.handleError("fetchUserCoords", [])));
+  }
+
+  getOneCoords(id: number): Observable<Coords> {
+    return this.http
+      .get<Coords>(`${this.coordsUrl}/${id}`, this.httpOptions)
+      .pipe(catchError(this.handleError<Coords>(`find coords id=${id}`)));
+  }
+
+  updateCoords(id: number, coords: Coords): Observable<any> {
+    return this.http
+      .put(`${this.coordsUrl}/${id}`, coords, this.httpOptions)
+      .pipe(catchError(this.handleError<Coords>("updateCoords")));
+  }
+
+  deleteCoords(id: number): Observable<Coords> {
+    return this.http
+      .delete<Coords>(`${this.coordsUrl}/${id}`, this.httpOptions)
+      .pipe(catchError(this.handleError<Coords>("deleteCoords")));
+  }
+}
--- src/app/new-housing/new-housing.component.ts
+++ src/app/new-housing/new-housing.component.ts
@@ -1,31 +1,68 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from "@angular/core";
 import { ListingService } from "../listing.service";
 import { Listing } from "../listing";
 
 @Component({
-  selector: 'app-new-housing',
-  templateUrl: './new-housing.component.html',
-  styleUrls: ['./new-housing.component.css']
+  selector: "app-new-housing",
+  templateUrl: "./new-housing.component.html",
+  styleUrls: ["./new-housing.component.css"]
 })
-export class NewHousingComponent implements OnInit {
+export class NewHousingComponent {
+  startingLat = 39.7684;
+  startingLng = -86.1581;
+  lat = "";
+  lng = "";
 
-  constructor(private listingService: ListingService) { }
+  constructor(private listingService: ListingService) {}
 
-  submitListing(residenceType: string, rooms: string, bathrooms: string, address: string, petsAllowed: string, facilities: string, price: string) : void {
-    this.listing(residenceType, rooms, bathrooms, address, petsAllowed, facilities, price)
+  onPickedLocation(event) {
+    this.lat = event.coords.lat;
+    this.lng = event.coords.lng;
+    console.log(this.lat, this.lng);
   }
 
-  listing(residenceType: string, rooms: string, bathrooms: string, address: string, petsAllowed: string, facilities: string, price: string) : void {
+  submitListing(
+    residenceType: string,
+    rooms: string,
+    bathrooms: string,
+    address: string,
+    petsAllowed: string,
+    facilities: string,
+    price: string
+  ): void {
+    this.listing(
+      residenceType,
+      rooms,
+      bathrooms,
+      address,
+      petsAllowed,
+      facilities,
+      price
+    );
+  }
+
+  listing(
+    residenceType: string,
+    rooms: string,
+    bathrooms: string,
+    address: string,
+    petsAllowed: string,
+    facilities: string,
+    price: string
+  ): void {
     this.listingService
-    .listing({residenceType, rooms, bathrooms, address, petsAllowed, facilities, price} as Listing)
-    .subscribe( results => {
-      alert("Listing Added")
-      console.log(results)
-    })
+      .listing({
+        residenceType,
+        rooms,
+        bathrooms,
+        address,
+        petsAllowed,
+        facilities,
+        price
+      } as Listing)
+      .subscribe(results => {
+        alert("Listing Added");
+        console.log(results);
+      });
   }
-
-  ngOnInit() {
-  }
-
 }
-  
