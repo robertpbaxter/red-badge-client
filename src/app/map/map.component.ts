2ac92c7ee3d9012c77d5ead08b815d8cc8d29cd1
Robert <roer.baxter@gmail.com>
2018-11-01 19:41:14 -0400

    rendering coordinates on home page

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -1,55 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { Coords } from "../coords";
+import { CoordsService } from "../coords.service";
 
 @Component({
-  selector: 'app-map',
-  templateUrl: './map.component.html',
-  styleUrls: ['./map.component.css']
+  selector: "app-map",
+  templateUrl: "./map.component.html",
+  styleUrls: ["./map.component.css"]
 })
 export class MapComponent implements OnInit {
-  title: 'Map'
-  startingLat = 39.7684
-  startingLng = -86.1581
-  points= [
-    {
-    
-      lat: 38.7684,
-      lng: -87.158
-    },
-    {
-      lat: 39.7684,
-      lng: -86.1570
-    },
-    {
-      lat: 39.50,
-      lng: -86.1570
-    },
-    {
-      lat: 39.301,
-      lng: -86.1570
-    },
-    {
-      lat: 39.9,
-      lng: -86.1570
-    }
-  ]
+  coords: Coords[];
+  startingLat = 39.7684;
+  startingLng = -86.1581;
 
-
-
-  constructor() { }
+  constructor(private coordsService: CoordsService) {}
 
   ngOnInit() {
-
-
-  }
-  onPickedLocation(event) {
-    console.log(this.points);
-    console.log(this.points[0])
-    this.points[0].lat= event.coords.lat
-    this.points[0].lng= event.coords.lng
-    // this.lat = event.coords.lat;
-    // this.lng = event.coords.lng;
+    this.fetchCoordinates();
   }
 
+  fetchCoordinates(): void {
+    this.coordsService.getAllCoords().subscribe(coords => {
+      console.log(coords);
+      this.coords = coords;
+    });
+  }
 }
-
-// AIzaSyBjo_g0gV5qXLS1ASDwVNCqvh0M9_IrZsM
