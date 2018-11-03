7915088cae00005e5b9c233dbedfcaa568f5ff07
Robert <roer.baxter@gmail.com>
2018-11-03 12:08:42 -0400

    trying the node method again

--- server.js
+++ server.js
@@ -6,7 +6,8 @@ const app = express();
 app.use(express.static(`${__dirname}/dist/nest-capstone-project`));
 
 app.get("/*", function(req, res) {
-  res.sendFile(path.join(`${__dirname}/dist/nest-capstone-project/index.html`));
+  res.send("Accessing angular server.js");
+  // res.sendFile(path.join(`${__dirname}/dist/nest-capstone-project/index.html`));
 });
 
 app.listen(process.env.PORT || 4200);
