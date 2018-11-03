3909340b122773e420e9e05c08787131e805fec3
Robert <roer.baxter@gmail.com>
2018-11-03 11:48:57 -0400

    trying the node method again

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ const path = require("path");
 
 const app = express();
 
-app.use(express.static(__dirname + "/dist/nest-capstone-project"));
+app.use(express.static(`${__dirname}/dist/nest-capstone-project`));
 
 app.get("/*", function(req, res) {
-  res.sendFile(path.join(__dirname + "/dist/nest-capstone-project/index.html"));
+  res.sendFile(`./dist/nest-capstone-project/index.html`);
 });
 
 app.listen(process.env.PORT || 4200);
