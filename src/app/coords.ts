2e6885389ad9f3c8fa12638b229fad860b649e4c
Robert <roer.baxter@gmail.com>
2018-11-03 10:42:57 -0400

    still working on first deployment

--- src/app/coords.ts
+++ src/app/coords.ts
@@ -3,6 +3,8 @@ export class Coords {
     public id: number,
     public housingId: number,
     public lat: number,
-    public lng: number
+    public lng: number,
+    public latitude: number,
+    public longitude: number
   ) {}
 }
