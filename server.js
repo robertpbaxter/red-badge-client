f393de4d576e358f6e48c2919d7815c7d200e98c
Robert <roer.baxter@gmail.com>
2018-11-03 11:53:06 -0400

    trying the node method again

--- server.js
+++ server.js
@@ -1,11 +1,10 @@
 const express = require("express");
-const path = require("path");
 
 const app = express();
 
 app.use(express.static(`${__dirname}/dist/nest-capstone-project`));
 
-app.get("/*", function(req, res) {
+app.get("*", function(req, res) {
   res.sendFile(`./dist/nest-capstone-project/index.html`);
 });
 
