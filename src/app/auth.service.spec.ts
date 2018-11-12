15fa276fd2ff239aa6991bf71b8965c823b74db1
Robert <roer.baxter@gmail.com>
2018-11-11 20:52:25 -0500

    changing a baseline in spec files

--- src/app/auth.service.spec.ts
+++ src/app/auth.service.spec.ts
@@ -2,7 +2,7 @@ import { TestBed } from "@angular/core/testing";
 
 import { AuthService } from "./auth.service";
 
-describe("Auth1Service", () => {
+describe("AuthService", () => {
   beforeEach(() => TestBed.configureTestingModule({}));
 
   it("should be created", () => {
