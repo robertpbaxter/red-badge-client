50fb1c4a54bd1b93a5c342a130bdbc8afa85227f
Robert P Baxter, Ph.D <40548269+robertpbaxter@users.noreply.github.com>
2018-11-14 14:44:41 -0500

    kitchen sink email validator

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -20,7 +20,6 @@ export class SignupComponent implements OnInit {
   ) {
     let e = email.toLowerCase().trim();
     let p = password.trim();
-    // let mailFormat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
     switch (true) {
       case firstName === "":
         alert("First name cannot be blank");
@@ -28,9 +27,9 @@ export class SignupComponent implements OnInit {
       case lastName === "":
         alert("Last name cannot be blank");
         break;
-      // case email.match(mailFormat):
-      //   alert("Email matches!");
-      //   break;
+      case this.validateEmail(email) === false:
+        alert("Email must be a valid email");
+        break;
       case password.length < 8:
         alert("Password must be at least 8 characters long");
         break;
@@ -40,6 +39,11 @@ export class SignupComponent implements OnInit {
       default:
         this.signup(firstName, lastName, e, p);
     }
+  }
+
+  validateEmail(email: string): Boolean {
+    let re = /^(?:[a-z0-9!#$%&amp;'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&amp;'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])$/;
+    return re.test(email);
   }
 
   signup(
